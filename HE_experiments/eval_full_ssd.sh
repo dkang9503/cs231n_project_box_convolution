21d5e625062555d44f8ad86473cf638a3dda1ed4
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-06-05 05:47:57 +0000

    full_box_ssd built.

---
+++ HE_experiments/eval_full_ssd.sh
@@ -0,0 +1,4 @@
+python3 ../ssd.pytorch/eval.py\
+	--trained_model ~/Project/Code/weights/1.0VOCfull_box_ssd_lr0.001_wd0.0005_sub1.0.pth\
+	--save_folder ~/Project/Code/weights/\
+	--voc_root ~/Project/Code/data/VOCdevkit/\
