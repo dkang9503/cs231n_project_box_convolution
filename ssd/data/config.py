706491ecb5eefcdcb71ffe1cca0cb7aceed40947
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-04-22 22:56:22 +0000

    Complete migration of ssd

---
+++ ssd/data/config.py
@@ -0,0 +1,47 @@
+# config.py
+import os.path
+
+# gets home dir cross platform
+HOME = os.path.expanduser("~")
+
+# for making bounding boxes pretty
+COLORS = ((255, 0, 0, 128), (0, 255, 0, 128), (0, 0, 255, 128),
+          (0, 255, 255, 128), (255, 0, 255, 128), (255, 255, 0, 128))
+
+#MEANS = (104, 117, 123)
+#MEANS = (77.6040, 75.4687, 72.7145) # VOC 2012 trainval means
+#MEANS = (0,0,0)
+MEANS = (77.4231, 75.1921, 72.1491) #VOC 2012 trainval means 10% sample
+
+# SSD300 CONFIGS
+voc = {
+    'num_classes': 21,
+    #'lr_steps': (80000, 100000, 120000),
+    'lr_steps': (1200, 2400, 4800),
+    #'max_iter': 120000,
+    'max_iter': 50000,
+    'feature_maps': [38, 19, 10, 5, 3, 1],
+    'min_dim': 300,
+    'steps': [8, 16, 32, 64, 100, 300],
+    'min_sizes': [30, 60, 111, 162, 213, 264],
+    'max_sizes': [60, 111, 162, 213, 264, 315],
+    'aspect_ratios': [[2], [2, 3], [2, 3], [2, 3], [2], [2]],
+    'variance': [0.1, 0.2],
+    'clip': True,
+    'name': 'VOC',
+}
+
+coco = {
+    'num_classes': 201,
+    'lr_steps': (280000, 360000, 400000),
+    'max_iter': 400000,
+    'feature_maps': [38, 19, 10, 5, 3, 1],
+    'min_dim': 300,
+    'steps': [8, 16, 32, 64, 100, 300],
+    'min_sizes': [21, 45, 99, 153, 207, 261],
+    'max_sizes': [45, 99, 153, 207, 261, 315],
+    'aspect_ratios': [[2], [2, 3], [2, 3], [2, 3], [2], [2]],
+    'variance': [0.1, 0.2],
+    'clip': True,
+    'name': 'COCO',
+}
---
+++ ssd/utils/metrics.py
@@ -0,0 +1,25 @@
+'''
+scores() is used in ssd to get mPA, IOU and other metrics
+'''
+
+def scores(label_trues, label_preds, n_class):
+    hist = np.zeros((n_class, n_class))
+    for lt, lp in zip(label_trues, label_preds):
+        hist += _fast_hist(lt.flatten(), lp.flatten(), n_class)
+    acc = np.diag(hist).sum() / hist.sum()
+    acc_cls = np.diag(hist) / hist.sum(axis=1)
+    acc_cls = np.nanmean(acc_cls)
+    iu = np.diag(hist) / (hist.sum(axis=1) + hist.sum(axis=0) - np.diag(hist))
+    valid = hist.sum(axis=1) > 0  # added
+    mean_iu = np.nanmean(iu[valid])
+    freq = hist.sum(axis=1) / hist.sum()
+    fwavacc = (freq[freq > 0] * iu[freq > 0]).sum()
+    cls_iu = dict(zip(range(n_class), iu))
+
+    return {
+        "Pixel Accuracy": acc,
+        "Mean Accuracy": acc_cls,
+        "Frequency Weighted IoU": fwavacc,
+        "Mean IoU": mean_iu,
+        "Class IoU": cls_iu,
+    }
